1ae98aa57d2200805e6b8233dce478292ca59382
raid-salhi <118809948+raid-salhi@users.noreply.github.com>
2024-05-23 13:39:13 +0100

    Created dataclass

---
+++ app/src/main/java/com/example/simplemovieapp/data/model/Movie.kt
@@ -0,0 +1,9 @@
+package com.example.simplemovieapp.data.model
+
+//this data class holds only necessary information about the movie in the app
+data class Movie(
+    val id: Int,
+    val title: String,
+    val year: Int,
+    val description: String
+)
